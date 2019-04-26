5b3f6ed46d07480b2a60e360b7085163cea60635
littltprince <1124479307@qq.com>
2019-04-26 14:29:42 +0800

    循环

---
+++ python-grammar/xunhuan-day3.py
@@ -0,0 +1,28 @@
+#循环的两种形式:
+# 一：for ** in **,print
+#二：while ** ，do,break,continue
+#encoding:utf-8
+# names=['1','2','3']
+# for name in names:
+#     print(name)
+
+# sum=0
+# for x in list(range(101)):
+#     sum=sum+x
+# print(sum)
+
+# l=['a','b','c']
+# for name in l:
+#     print('hello',name,'!')
+
+# n1=255
+# n2=1000
+# print(hex(n1))
+# print(hex(n2))
+
+def myabs(c):
+    if c>=0:
+       return c
+    else:
+        return -c
+print(myabs(-90))
